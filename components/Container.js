6620c4afab683856cafc82daf7da7de0430b6593
1smat <reven13022002@gmail.com>
2021-08-24 20:56:58 +0500

    done

--- components/Container.js
+++ components/Container.js
@@ -1,6 +1,9 @@
+import Link from 'next/link'
+import Image from 'next/image'
 import Linker from './Linker'
 import Head from 'next/head'
 import styles from '../styles/Container.module.scss'
+import logo from '../public/logo.png'
 
 const MainContainer = ({ children, keywords }) => {
   return (
@@ -10,10 +13,14 @@ const MainContainer = ({ children, keywords }) => {
         <title>Главная страница</title>
       </Head>
       <div className={styles.navbar}>
-        <Linker href={'/'} text='Главная' />
-        <Linker href={'/top-rated'} text='Top rated' />
-        <Linker href={'/popular'} text='Popular' />
-        <Linker href={'/contact'} text='Связь с нами' />
+        <div className={styles.nav_cont}>
+          <Link className={styles.logo} href='/'>
+            <a className={styles.logo}></a>
+          </Link>
+          <Linker href={'/top-rated'} text='Top rated' />
+          <Linker href={'/popular'} text='Popular' />
+          <Linker href={'/contact'} text='Связь с нами' />
+        </div>
       </div>
       <div>{children}</div>
     </>
