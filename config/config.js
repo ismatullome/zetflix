8c9f65281316ddcb6d38761a5f0425bdeae5dc0a
1smat <reven13022002@gmail.com>
2021-08-24 19:05:41 +0500

    done

---
+++ config/config.js
@@ -0,0 +1,2 @@
+const API_KEY = 'aa854f51f9dc68905a36b3fa6d67eb60'
+export default API_KEY
--- pages/users.js
+++ pages/users.js
@@ -2,33 +2,73 @@ import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Container from '../components/Container'
 import styles from '../styles/Users.module.scss'
+import key from '../config/config'
+import Image from 'next/image'
 
-const Users = ({ users }) => {
+// const imgSite = 'http://image.tmdb.org/t/p/w500/'
+
+export async function getStaticProps() {
+  const res = await fetch(
+    `http://api.themoviedb.org/3/movie/top_rated?api_key=${key}`
+  )
+  const movies = await res.json()
+  // console.log(post.results.id)
+  // console.log(res)
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      movies,
+    },
+  }
+}
+function Movies({ movies }) {
   return (
-    <Container keywords={'users ism4t'}>
-      <div className={styles.users}>
-        <h1>Cписок пользователей</h1>
-        <ul>
-          {users.map((user) => (
-            <li className={styles.user} key={user.id}>
-              <Link href={`/users/${user.id}`}>
-                <a className={styles.user_link}>{user.name}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
+    <>
+      <div className={styles.container}>
+        {movies.results.map((movie) => (
+          <div className={styles.movie}>
+            <img
+              className={styles.img}
+              width='282'
+              height='160'
+              src={`http://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+              alt='Picture of the author'
+            />
+            <h1 className={styles.title}>{movie.title}</h1>
+            <p className={styles.desc}>{movie.overview}</p>
+          </div>
+        ))}
       </div>
-    </Container>
+    </>
   )
 }
 
-export default Users
+export default Movies
 
-export async function getStaticProps(context) {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/users`)
-  const users = await response.json()
+// export async function getStaticProps(context) {
+//   const response = await fetch(
+//     `http://api.themoviedb.org/3/movie/top_rated?api_key=${key}`
+//   )
+//   const movies = await response.json()
+//   // console.log(movies)
+//   const data = movies.results
+//   const imgUrl = movies.results[1].backdrop_path
 
-  return {
-    props: { users }, // will be passed to the page component as props
-  }
-}
+//   return {
+//     props: { data }, // will be passed to the page component as props
+//   }
+// }
+
+// const Users = ({ data }) => {
+//   return (
+//     <Container keywords={'users ism4t'}>
+//       <div className={styles.users}>
+//         <ul> {JSON.stringif(imgUrl)} </ul>
+//       </div>
+//     </Container>
+//   )
+// }
+
+// export default Users
