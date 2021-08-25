7d2c7a4598dd74390844e57ff192c2a5b7f73282
1smat <reven13022002@gmail.com>
2021-08-25 12:53:47 +0500

    content for each movie

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -3,9 +3,30 @@ import Container from '../../components/Container'
 
 export default function Movieid() {
   const query = useRouter()
-  console.log(query)
+  const results = query.components['/'].props.pageProps.movies.results
   return (
     <Container>
+      <div>
+        {/* {results.map((movie) => {
+          if( movie.id == query.query.id ) {
+            console.log(123);
+          }
+        }
+      } */}
+        {results.map((movie) => {
+          return movie.id == query.query.id ? (
+            <>
+              <div> movie name {movie.original_title}</div>
+              <img
+                src={`http://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                alt=''
+              />
+            </>
+          ) : (
+            <></>
+          )
+        })}
+      </div>
       <div>
         <h1>Id movie {query.query.id} </h1>
       </div>
