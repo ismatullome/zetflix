3ead87960585541cad3860a723a1a049a2192a7b
1smat <reven13022002@gmail.com>
2021-08-26 23:31:24 +0500

    done

--- pages/404.js
+++ pages/404.js
@@ -6,16 +6,21 @@ import Container from '../components/Container'
 
 export default function Error() {
   return (
-    <Container keywords={'users ism4t'}>
+    <>
       <div className={styles.div}>
-        <Linker
-          className={styles.link}
-          href={'/'}
-          text='Вернуть главную страницу'
-        />
         <a href='https:/t.me/ism4t'>Developer</a>
-        <Image className={styles.img} src={logo} height={600} width={1000} />
       </div>
-    </Container>
+      <Container keywords={'users ism4t'}>
+        <div className={styles.div}>
+          <Linker
+            className={styles.link}
+            href={'/'}
+            text='Вернуть главную страницу'
+          />
+
+          <Image className={styles.img} src={logo} height={600} width={1000} />
+        </div>
+      </Container>
+    </>
   )
 }
